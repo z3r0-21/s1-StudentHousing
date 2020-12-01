9ad66c74bb9395915161d9ff53b081b57a84bf2b
Antonio <454917@student.fontys.nl>
2020-12-01 20:04:34 +0100

    Add the main program to this branch in order to try to modify in way the program is going to have registration and login working together

---
+++ HousingSoftware/HousingSoftware/HomePage.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HousingSoftware
+{
+    partial class HomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbHello = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbHello
+            // 
+            this.lbHello.AutoSize = true;
+            this.lbHello.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHello.Location = new System.Drawing.Point(346, 171);
+            this.lbHello.Name = "lbHello";
+            this.lbHello.Size = new System.Drawing.Size(62, 25);
+            this.lbHello.TabIndex = 0;
+            this.lbHello.Text = "Hello!";
+            // 
+            // HomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbHello);
+            this.Name = "HomePage";
+            this.Text = "HomePage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbHello;
+    }
+}
